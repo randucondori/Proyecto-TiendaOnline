a6750aaf338a7d64c4e1fe555b1ba564f4a81cfb
randucondori <randucondoribendezur@gmail.com>
2026-02-04 12:23:24 +0100

    bootstrap js con ayuda de camlio

--- TiendaCompraYvende/src/app/layouts/main-layout/main-layout.ts
+++ TiendaCompraYvende/src/app/layouts/main-layout/main-layout.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {HeadMain} from '../components/head-main/head-main';
+import {HeadMain} from './components/head-main/head-main';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
