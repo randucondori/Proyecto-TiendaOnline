dd7a6505457db21dc944a0392a4962bbeae3bef2
randucondori <randucondoribendezur@gmail.com>
2026-02-05 14:13:24 +0100

    tarea: hacer feature con la tabla de los productos

--- TiendaCompraYvende/src/app/layouts/main-layout/main-layout.ts
+++ TiendaCompraYvende/src/app/layouts/main-layout/main-layout.ts
@@ -1,12 +1,14 @@
 import {Component, signal} from '@angular/core';
 import {HeadMain} from './components/head-main/head-main';
 import {RouterOutlet} from '@angular/router';
+import {BarraLateral} from './components/barra-lateral/barra-lateral';
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     HeadMain,
-    RouterOutlet
+    RouterOutlet,
+    BarraLateral
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
