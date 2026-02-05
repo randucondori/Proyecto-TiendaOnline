ca00f9c2a15d4bb30e0c067fb4234cfc57ccf241
randucondori <randucondori@gmail.com>
2026-02-05 18:27:27 +0100

    html de panel de control

--- TiendaCompraYvende/src/app/app.routes.ts
+++ TiendaCompraYvende/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {path:"",
     loadComponent:()=>import("./layouts/main-layout/main-layout").then((c)=>c.MainLayout)
-
+  },
+  {
+    path:"productosAdmin",
+    loadComponent:()=>import("./features/productos-admin/productos-admin").then(c=>c.ProductosAdmin)
   }
 ];
