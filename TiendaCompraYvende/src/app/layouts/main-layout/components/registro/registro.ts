65c439e3b81d39a2fbb073520185167e4a4286f1
randucondori <randucondori@gmail.com>
2026-02-12 19:52:23 +0100

    configuracion del formulario reactivo registro

--- TiendaCompraYvende/src/app/layouts/main-layout/components/registro/registro.ts
+++ TiendaCompraYvende/src/app/layouts/main-layout/components/registro/registro.ts
@@ -1,10 +1,12 @@
-import {Component} from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {ValidandoEmail} from '../../../../core/validators/email.validator';
 import {NgClass} from '@angular/common';
 import {PassValid} from '../../../../core/validators/pass.validator';
 import {IniciaCon} from '../inicia-con/inicia-con';
+import {LoginService} from '../../../../core/services/login/login.service';
+import {CargandoModel} from '../../../../shared/models/cargando-model/cargando-model';
 
 
 @Component({
@@ -13,29 +15,66 @@ import {IniciaCon} from '../inicia-con/inicia-con';
     RouterLink,
     IniciaCon,
     ReactiveFormsModule,
-    NgClass
+    NgClass,
+    CargandoModel
   ],
   templateUrl: './registro.html',
   styleUrl: './registro.scss',
 })
 export class Registro {
 
-  formRegister:FormGroup;
+  formRegister: FormGroup;
+  visibleCargando = signal<boolean>(false)
+  tipeResp = signal<number>(0)
+  errors = signal<any>([])
 
   constructor(
     private formBuilder: FormBuilder,
+    private router: Router,
+    private loginService: LoginService,
   ) {
-    this.formRegister= this.formBuilder.group({
-      nombre:['', [Validators.required]],
-      email: ['', [Validators.required,ValidandoEmail]],
-      password: ['', [Validators.required,PassValid]],
+    this.formRegister = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      email: ['', [Validators.required, ValidandoEmail]],
+      password: ['', [Validators.required, PassValid]],
       apellidos: ['', [Validators.required]],
-      password_confirmation: ['', [Validators.required,PassValid]],
+      password_confirmation: ['', [Validators.required, PassValid]],
       ciudad: ['', [Validators.required]],
     })
   }
 
+  Registrarse() {
 
+    let form = this.formRegister.value;
+
+    this.visibleCargando.set(true)
+
+    let data = {
+      "email": form.email,
+      "password": form.password,
+      "nombre": form.nombre,
+      "apellidos": form.apellidos,
+      "password0": form.password_confirmation,
+      "ciudad": form.ciudad,
+    };
+
+    this.loginService.ToRegister(data).subscribe({
+      next: () => {
+        this.tipeResp.set(1)
+        setTimeout(() => {
+          this.router.navigateByUrl('/productosAdmin');
+        }, 500)
+      },
+      error: (error) => {
+        this.tipeResp.set(2);
+        console.log(error)
+        this.errors.set(error.error);
+      },
+      complete: () => {
+        this.visibleCargando.set(false)
+      }
+    })
+  }
 
 
 }
