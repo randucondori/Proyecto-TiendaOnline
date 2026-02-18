6f7c3f7439455d9fcc3363f5e930670edac52cf6
randucondori <randucondoribendezu@gmail.com>
2026-02-18 14:18:20 +0100

    Aumento de plantilla con notificaciones hecha con ayuda de camilo :v

--- TiendaCompraYvende/src/app/features/explorer/explorer.ts
+++ TiendaCompraYvende/src/app/features/explorer/explorer.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
+import {ProductosService} from '../../core/services/Productos/productos.service';
+import {Observable} from 'rxjs';
+
+type producto = [{
+  nombre: string,
+  precio: number,
+  categoria_nombre: string,
+  slug: string,
+  img: string,
+}] | [];
 
 @Component({
   selector: 'app-explorer',
@@ -6,6 +16,34 @@ import { Component } from '@angular/core';
   templateUrl: './explorer.html',
   styleUrl: './explorer.scss',
 })
-export class Explorer {
+export class Explorer implements OnInit {
+
+  notificaciones = signal<string[]>([])
+  productos = signal<producto>([])
+
+  constructor(private getproductos: ProductosService) {
+  }
+
+  ngOnInit() {
+    this.getproductos.getProductos().subscribe({
+      next: data => {
+        this.productos.set(data.productos)
+      },
+      error: err => {
+
+      }
+    })
+  }
+
+  comprar(nombre: string) {
+    this.notificaciones.update(val => [...val, nombre]);
+
+    setTimeout(() => {
+      this.notificaciones.update(val=>[...val.slice(0,-1)])
+      console.log(this.notificaciones())
+      console.log("eliminado")
+    }, 2000)
+
+  }
 
 }
