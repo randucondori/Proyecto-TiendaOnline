edbe673986068e964094d50cedf0656959723f1b
randucondori <randucondori@gmail.com>
2026-02-20 22:50:54 +0100

    minimalismo en user-page

--- TiendaCompraYvende/src/app/features/user-page/user-page.ts
+++ TiendaCompraYvende/src/app/features/user-page/user-page.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import {PedidosUser} from './components/pedidos-user/pedidos-user';
-import {CategoriasUser} from './components/categorias-user/categorias-user';
 
 @Component({
   selector: 'app-user-page',
   imports: [
     PedidosUser,
-    CategoriasUser
   ],
   templateUrl: './user-page.html',
   styleUrl: './user-page.scss',
